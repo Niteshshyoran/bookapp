83f99e7055bf495ab79b6373241c15cf2a66328f
Niteshshyoran <niteshkumarshyoran9660@gmail.com>
2024-08-04 17:34:32 +0530

    adding viewsurl

--- booklibrary/book/urls.py
+++ booklibrary/book/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,include
-from .views import RegisterView
+from .views import RegisterView,loginView,AuthorRegisterView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(),name= 'register')
+    path('register/', RegisterView.as_view(),name= 'register'),
+    path('login/', loginView.as_view(),name= 'login'),
+    path('author/', AuthorRegisterView.as_view(),name= 'author'),
+    path('author/<int:pk>/update/', AuthorRegisterView.as_view(),name= 'authorupdate'),
+    path('book/', AuthorRegisterView.as_view(),name= 'book'),
+    path('book/<int:pk>/update/', AuthorRegisterView.as_view(),name= 'bookupdate'),
+    path('country/', AuthorRegisterView.as_view(),name= 'country'),
 ]
--- booklibrary/book/views.py
+++ booklibrary/book/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from rest_framework.response import Response
-from .serializer import UserSerializer
+
+from .models import Author,Books,Country
+from .serializer import UserSerializer,AuthorSerializer,BookSerializer,CountrySerializer
 from django.http import HttpResponse
 import datetime
+# from django.contrib.auth import login,logout,authenticate
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
 # Create your views here.
 
 
@@ -17,4 +21,44 @@ class RegisterView(APIView):
             return Response({'message': 'sign up sucessfull'})
         return Response ({'message':"invalid input"})
     
+
+class loginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = User.objects.all
+        if user is None:
+            return Response({'message':'user not register'})
+        # if not user.cheak_password(password):
+        return Response({'message':'login sucessfull'})
+
+class AuthorRegisterView(CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+
+class AuthorUpdateView(RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+
+class BookRegisterView(CreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+
+
+
+class BookUpdateView(RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+
+
+
+class CountryRegisterView(CreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
 
