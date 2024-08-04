0125b457a12edcb876860a9950e9b27c25aa9786
Niteshshyoran <niteshkumarshyoran9660@gmail.com>
2024-08-04 16:10:38 +0530

    adding serializer

---
+++ booklibrary/book/serializer.py
@@ -0,0 +1,41 @@
+from tokenize import Comment
+from rest_framework import serializers
+from models import Author,Books,Country
+from django.contrib.auth import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+            return Comment.objects.create(**validated_data)
+
+    
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        instance.save()
+        return instance
+    
+
+class AuthorSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Author
+          fields = '__all__'
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Books
+          fields = '__all__'
+
+
+class CountrySerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Author
+          fields = '__all__'
+          
